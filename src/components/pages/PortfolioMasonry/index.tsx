e144028b11d391575ccf673d419d84f2308ef9f6
IT21204302 <99412150+IT21204302@users.noreply.github.com>
2024-12-20 08:51:31 +0530

    add nav and footer

--- src/components/pages/PortfolioMasonry/index.tsx
+++ src/components/pages/PortfolioMasonry/index.tsx
@@ -4,18 +4,17 @@ import { Container } from 'react-bootstrap';
 import BackToTop from 'components/BackToTop';
 
 import Gallery from './Gallery';
-import { galleryItems } from './data';
 
 const PortfolioMasonry = () => {
     return (
         <>
             {/* gallery */}
-            <section className="overflow-hidden py-5 py-md-6 py-lg-7">
+            {/* <section className="overflow-hidden py-5 py-md-6 py-lg-7">
                 <Container>
                     <Gallery />
                 </Container>
-            </section>
-
+            </section> */}
+            <Gallery />
 
             <BackToTop />
         </>
